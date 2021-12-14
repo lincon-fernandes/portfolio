65201e5f82a6320f47ade88cdde1b12d68d6afd8
lincon-fernandes <lincon3013@gmail.com>
2021-12-13 21:25:04 -0300

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,24 @@ import avatar from '../img/avatar.jpg';
 import ReactTyped from 'react-typed';
 import { makeStyles } from '@material-ui/core/styles';
 //CSS STYLES
+
 const useStyles = makeStyles((theme) => ({
+  '@keyframes animate': {
+    '0%': {
+      filter: 'hue-rotate(0deg)',
+    },
+    '100%': {
+      filter: 'hue-rotate(360deg)',
+    },
+  },
+
+  avatarBox: {
+    position: 'absolute',
+    top: '45vh',
+    right: 'calc(50% - 200px)',
+    zIndex: 10000,
+    width: '400px',
+  },
   avatar: {
     width: theme.spacing(15),
     height: theme.spacing(15),
@@ -14,6 +31,57 @@ const useStyles = makeStyles((theme) => ({
   },
   subtitle: {
     color: '#fff',
+  },
+  container: {
+    position: 'relative',
+    height: '90vh',
+    overflow: 'hidden',
+    animation: '$animate 4s linear infinite',
+    background: '#000',
+  },
+
+  row: {
+    display: 'inline-flex',
+    marginTop: '-32px',
+    marginLeft: '-50px',
+    '&:nth-child(even)': {
+      marginLeft: '1px',
+    },
+  },
+  hexagon: {
+    position: 'relative',
+    width: '100px',
+    height: '110px',
+    background: '#111',
+    margin: '1px',
+    clipPath: 'polygon(50% 0%, 100% 25%,100% 75%,50% 100%,0% 75%, 0% 25%)',
+    transition: '2s',
+    '&:hover': {
+      transition: '0s',
+      background: '#0f0',
+    },
+    '&:after': {
+      content: '',
+      position: 'absolute',
+      top: '4px',
+      bottom: '4px',
+      left: '4px',
+      right: '4px',
+      background: '#111',
+      clipPath: 'polygon(50% 0%, 100% 25%, 100% 75%,50% 100%, 0% 75%, 0% 25%)',
+      zIndex: 1,
+    },
+    '&:before': {
+      content: '',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '50%',
+      height: '100%',
+      pointerEvents: 'nome',
+      background: 'rgba(255,255,255,0.02',
+      zIndex: 1000,
+    },
   },
   typedContainer: {
     display: 'flex',
@@ -25,12 +93,9 @@ const useStyles = makeStyles((theme) => ({
     paddingTop: '1rem',
     marginBottom: '1rem',
     height: '400px',
-
-    backgroundImage:
-      'url(https://images.freeimages.com/images/large-previews/443/php-code-1242330.jpg)',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center center',
+  },
+  reactTyped: {
+    textAlign: 'center',
   },
 }));
 
@@ -38,30 +103,300 @@ const Header = () => {
   const classes = useStyles();
   return (
     <Box component="div" className={classes.typedContainer}>
-      <Grid container justifyContent="center">
-        <Avatar
-          className={classes.avatar}
-          src={avatar}
-          alt="Lincon Fernandes"
-        ></Avatar>
-      </Grid>
-
-      <Typography className={classes.title} variant="h4">
-        <ReactTyped strings={['Lincon Fernandes']} typedSpeed={40} />
-      </Typography>
-      <br />
-      <Typography className={classes.subtitle} variant="h5">
-        <ReactTyped
-          strings={[
-            'React js',
-            'HTML, CSS JavaScript',
-            'Styled Components',
-            'material UI',
-          ]}
-          typedSpeed={40}
-          loop
-        />
-      </Typography>
+      <div className={classes.container}>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+        <div className={classes.row}>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+          <div className={classes.hexagon}></div>
+        </div>
+      </div>
+      <Box className={classes.infoContainer}>
+        <Grid
+          container
+          className={classes.avatarBox}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item>
+            <Avatar
+              className={classes.avatar}
+              src={avatar}
+              alt="Lincon Fernandes"
+            ></Avatar>
+          </Grid>
+          <Grid item justifyContent="center">
+            <Typography className={classes.title} variant="h4">
+              <ReactTyped strings={['Lincon Fernandes']} typedSpeed={40} />
+            </Typography>
+            <br />
+            <Typography
+              alignItems="center"
+              className={classes.subtitle}
+              variant="h5"
+            >
+              <ReactTyped
+                strings={[
+                  'React js',
+                  'HTML, CSS JavaScript',
+                  'Styled Components',
+                  'material UI',
+                ]}
+                typedSpeed={40}
+                loop
+                className={classes.reactTyped}
+              />
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
     </Box>
   );
 };
